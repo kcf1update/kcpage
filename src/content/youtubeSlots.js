d6306e0fb8b9958413041806bef492464003319b
Kevin <you@example.com>
2026-02-26 12:07:18 -0400

    news and youtube updates

--- src/content/youtubeSlots.js
+++ src/content/youtubeSlots.js
@@ -11,27 +11,27 @@ export const youtubeSlots = [
 
   {
     slotId: "slot1",
-    title: "2026 F1 McLaren vs Mercedes - Scarbs analysis by Peter Windsor",
-    youtubeInput: "https://youtu.be/sgahzgWedG4",
-    description: "No one will know until Australia!",
+    title: "Crofty explains why there might be CHAOS from the start as the new era of F1 begins!",
+    youtubeInput: "https://youtu.be/w8cd2xc3eEw",
+    description: "It sounds like the start of the race will be interesting",
     
   },
 
  
     {
   slotId: "slot2",
-  title: "Is This Adrian Newey's Worst Car Ever? | Peter Windsor Reacts",
-  youtubeInput: "https://youtu.be/WxxVmyCJHhw",
-  description: "To much pressure on Adrian Newey",
+  title: "Is Aston Martin DOOMED for the 2026 F1 season?",
+  youtubeInput: "https://youtu.be/zJkRzGvN51M",
+  description: "I'm not sure it was a disater but they are definitely in trouble, for the few races anyway.",
   
 },
   
 
   {
     slotId: "slot3",
-    title: "Ranking the 2026 F1 teams after pre-season testing",
-    youtubeInput: "https://youtu.be/TWwfDBRi0E4",
-    description: "Do you agree?",
+    title: "The F1 drivers facing serious trouble already in 2026",
+    youtubeInput: "https://youtu.be/dFmgx8A9V84",
+    description: "Is it just me or does a lot of the news seem negative this year?",
     
   }
 
