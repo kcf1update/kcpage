e2593b1d7e9abf402b6155e3f0f9857e7cda856b
Kevin <you@example.com>
2026-03-28 14:45:50 -0300

    fix youtube syntax 0328

--- src/content/youtubeSlots.js
+++ src/content/youtubeSlots.js
@@ -1,40 +1,31 @@
-import { buildQueries } from "@testing-library/dom";
+
 
 export const youtubeSlots = [
-
   {
     slotId: "slotKC",
-    title: "KC’s Channel (Subscribe)",
+    title: "KC's Channel (Subscribe)",
     youtubeInput: "https://www.youtube.com/shorts/E6fk5HBWigk?feature=share",
-    description: "Shock Exit: Wheatley Leaves Audi—Aston Martin Move Next?",
+    description: "Shock Exit: Wheatley Leaves Audi–Aston Martin Move Next?",
     ctaUrl: "https://www.youtube.com/shorts/E6fk5HBWigk?feature=share",
-    ctaLabel: "Visit KC’s YouTube →",
+    ctaLabel: "Visit KC's YouTube →",
   },
 
   {
     slotId: "slot1",
-    title: "Kimi Antonelli's Pole Lap | 2026 Japanese Grand Prix | Pirelli ",
+    title: "Kimi Antonelli's Pole Lap | 2026 Japanese Grand Prix | Pirelli",
     youtubeInput: "https://youtu.be/VT8ULSU6em8",
-   forceExternal :"blue"
-    
-    
+    forceExternal: "blue",
   },
 
- 
-    {
-  slotId: "slot2",
-  title: "Ted's Qualifying Notebook - Japanese Grand Prix 2026",
-  youtubeInput: "https://youtu.be/gqcO4OJ3uSQ",
-   
-},
-  
+  {
+    slotId: "slot2",
+    title: "Ted's Qualifying Notebook - Japanese Grand Prix 2026",
+    youtubeInput: "https://youtu.be/gqcO4OJ3uSQ",
+  },
 
   {
     slotId: "slot3",
     title: "2026 F1 Japanese GP qualifying analysis by Peter Windsor",
     youtubeInput: "https://youtu.be/G8ux9wQMsko",
-    
-     
   }
-
 ];
