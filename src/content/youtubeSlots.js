1c4d44a47b08d4518273b5ede79f8f3844791fb8
Kevin <you@example.com>
2026-04-07 10:28:23 -0300

    news and youtube updates 0704

--- src/content/youtubeSlots.js
+++ src/content/youtubeSlots.js
@@ -12,22 +12,22 @@ export const youtubeSlots = [
 
   {
     slotId: "slot1",
-    title: "Crofty answers 11 questions you've ALWAYS wanted to ask an F1 commentator! | F1 Unpacked",
-    youtubeInput: "https://youtu.be/fEjFCRGR718",
-     
+    title: "Rivals & Stars Of The F1 Season So Far | F1 Nation Podcast",
+    youtubeInput: "https://youtu.be/ylYWdzF2iXs",
+     forceExternal: "blue" 
   },
 
   {
     slotId: "slot2",
-    title: "Can Adrian Newey's F1 genius really save Aston Martin-Honda?",
-    youtubeInput: "https://youtu.be/UNGgOgWi6E8",
+    title: "Are the NEW F1 REGULATIONS good or bad for the sport?",
+    youtubeInput: "https://youtu.be/UCSOGCPD3NI",
     
   },
 
   {
     slotId: "slot3",
-    title: "Laurent Mekies: Relishing Red Bull Challenge | F1 Beyond The Grid Podcast",
-    youtubeInput: "https://youtu.be/ADjpL-YeW-k",
-    forceExternal: "blue" 
+    title: "F1 Can't Keep Doing This! | F1 News",
+    youtubeInput: "https://youtu.be/OQAm-yRV6KE",
+    
   }
 ];
