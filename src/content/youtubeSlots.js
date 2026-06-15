eaa32c1572cbc7ca984900934285328c002f7123
Kevin <you@example.com>
2026-06-15 19:35:59 -0300

    youtube update

--- src/content/youtubeSlots.js
+++ src/content/youtubeSlots.js
@@ -3,8 +3,8 @@
 export const youtubeSlots = [
   {
   slotId: "slotKC",
-  title: "2026 Canadian Grand Prix Recap: Antonelli Wins, McLaren Gamble Fails",
-  youtubeInput: "https://youtu.be/8x8L35waR9Q",
+  title: "Hamilton Wins For Ferrari In Barcelona | Antonelli Retires Late, Penalties Hit Post-Race",
+  youtubeInput: "https://youtu.be/TfQPf5_HCdM",
 },
 
   {
@@ -25,6 +25,6 @@ export const youtubeSlots = [
     slotId: "slot3",
     title: "Did Hamilton’s CAREER RISK finally pay off at the BARCELONA GRAND PRIX?",
     youtubeInput: "https://youtu.be/qqsqjql0VJI",
-    forceExternal: true,
+    
   }
 ];
